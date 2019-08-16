1b3495608dc6f98516cac21ab555c8d65c9a50c5
Egor Suvorov <esuvorov@think-cell.com>
2019-08-16 17:01:01 +0200

    examples: enable all warnings back, move Library/ to -isystem to ignore warnings

--- examples/CallbacksTest/build.cmd
+++ examples/CallbacksTest/build.cmd
@@ -3,10 +3,9 @@
     -g4 --source-map-base http://localhost:8080/ ^
     -s SAFE_HEAP=1 -s ASSERTIONS=2 -s WASM=1 ^
     -I ../../bootstrap/include ^
-    -I ../../../code/Library/HeaderOnly/Public ^
+    -isystem ../../../code/Library/HeaderOnly/Public ^
     -isystem c:/Libraries/boost_1_67_0 ^
     -Wall -Wextra -Werror ^
-    -Wno-unknown-pragmas -Wno-logical-op-parentheses ^
     --pre-js ../../bootstrap/src/js_callback.js ^
     ../../bootstrap/src/js_callback.cpp ^
     --pre-js main-pre.js ^
