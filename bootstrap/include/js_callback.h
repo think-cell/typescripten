f4cff0d11f4eeefa124394701f8f1d3741661c2a
Egor Suvorov <esuvorov@think-cell.com>
2019-08-16 14:43:42 +0200

    js_callback: add static typing like js_ref<IJsFunction<...>>

--- bootstrap/include/js_callback.h
+++ bootstrap/include/js_callback.h
@@ -15,6 +15,7 @@
 #include "tc_move.h"
 #include "type_list.h"
 #include <boost/callable_traits.hpp>
+#include "js_ref.h"
 
 namespace tc::js {
 // ---------------------------------------- Passing member functions to JS ----------------------------------------
@@ -38,6 +39,10 @@ struct CCallableWrapper final {
         }
         return CCallHelper<LArgs, std::make_index_sequence<tc::type::size<LArgs>::value>>()(std::forward<Fn>(fn), emvalArgs);
     }
+
+    using JsFunctionLArgs = LArgs;
+    using JsFunctionTThis = void;
+    using JsFunctionTArgs = void;
 
 private:
     template<typename, typename>
@@ -64,13 +69,19 @@ template<typename TThis, typename... ArgsTail, bool bPassedAllArguments>
 struct CCallableWrapper<tc::type::list<pass_this_t, TThis, ArgsTail...>, bPassedAllArguments> final {
 private:
     using LArgsTail = tc::type::list<ArgsTail...>;
+    using TailCCallableWrapper = CCallableWrapper<LArgsTail, bPassedAllArguments>;
 public:
     static_assert(!tc::type::contains<LArgsTail, pass_this_t>::value);
     static_assert(!std::is_reference<TThis>::value);
 
+    using JsFunctionLArgs = typename TailCCallableWrapper::JsFunctionLArgs;
+    static_assert(std::is_same<typename TailCCallableWrapper::JsFunctionTThis, void>::value);
+    using JsFunctionTThis = TThis;
+    using JsFunctionTArgs = typename TailCCallableWrapper::JsFunctionTArgs;
+
     template<typename Fn>
     emscripten::val operator()(Fn&& fn, emscripten::val const& emvalThis, emscripten::val const& emvalArgs) const& noexcept {
-        return CCallableWrapper<LArgsTail, bPassedAllArguments>()(
+        return TailCCallableWrapper()(
             [&](auto&&... args) noexcept {
                 return fn(pass_this, emvalThis.template as<TThis>(), std::forward<decltype(args)>(args)...);
             },
@@ -84,14 +95,20 @@ template<typename TArgs, typename... ArgsTail, bool bPassedAllArguments>
 struct CCallableWrapper<tc::type::list<pass_all_arguments_t, TArgs, ArgsTail...>, bPassedAllArguments> final {
 private:
     using LArgsTail = tc::type::list<ArgsTail...>;
+    using TailCCallableWrapper = CCallableWrapper<LArgsTail, /*bPassedAllArguments=*/true>;
 public:
     static_assert(!tc::type::contains<LArgsTail, pass_all_arguments_t>::value);
     static_assert(!std::is_reference<TArgs>::value);
     static_assert(!bPassedAllArguments);
 
+    using JsFunctionLArgs = typename TailCCallableWrapper::JsFunctionLArgs;
+    using JsFunctionTThis = typename TailCCallableWrapper::JsFunctionTThis;
+    static_assert(std::is_same<typename TailCCallableWrapper::JsFunctionTArgs, void>::value);
+    using JsFunctionTArgs = TArgs;
+
     template<typename Fn>
     emscripten::val operator()(Fn&& fn, emscripten::val const& emvalThis, emscripten::val const& emvalArgs) const& noexcept {
-        return CCallableWrapper<LArgsTail, /*bPassedAllArguments=*/true>()(
+        return TailCCallableWrapper()(
             [&](auto&&... args) noexcept {
                 return fn(pass_all_arguments, emvalArgs.template as<TArgs>(), std::forward<decltype(args)>(args)...);
             },
@@ -103,16 +120,29 @@ public:
 } // namespace no_adl
 using no_adl::CCallableWrapper;
 
+template<typename>
+struct CJsFunctionInterface {};
+
+template<typename FromR, typename... FromArgs>
+struct CJsFunctionInterface<FromR(FromArgs...)> {
+    using LFromArgs = tc::type::list<FromArgs...>;
+
+    template<typename... Args>
+    using ConstructSignature = FromR(Args...);
+
+    typedef IJsFunction<
+        tc::type::apply_t<ConstructSignature, typename CCallableWrapper<LFromArgs>::JsFunctionLArgs>,
+        typename CCallableWrapper<LFromArgs>::JsFunctionTThis,
+        typename CCallableWrapper<LFromArgs>::JsFunctionTArgs
+    > type;
+};
+
+template<typename T>
+using CJsFunctionInterface_t = typename CJsFunctionInterface<T>::type;
+
 template<typename Pmf, typename = tc::void_t<boost::callable_traits::class_of_t<Pmf>>>
 using MemberFunctionExpectedArgs_t =
     tc::type::drop_first_t<boost::callable_traits::args_t<Pmf, tc::type::list>>;
-
-template<typename Pmf>
-using MemberFunctionJsSignature_t =
-    boost::callable_traits::apply_return<
-        tc::type::apply<std::tuple, MemberFunctionExpectedArgs_t<Pmf>>,
-        boost::callable_traits::return_type<Pmf>
-    >;
 
 template<typename T>
 emscripten::val MemberFunctionWrapper(T pmfMember, void* pvThis, emscripten::val const& emvalThis, emscripten::val const& emvalArgs) noexcept {
@@ -133,41 +163,51 @@ using FunctionPointer = emscripten::val(*)(FirstArgument, emscripten::val const&
 using PointerNumber = std::uintptr_t;
 
 namespace no_adl {
-// We do not care about slicing to emscripten::val, because this class is
-// only stored as a by-value field.
-struct CUniqueDetachableEmscriptenVal : tc::nonmovable, emscripten::val {
-    CUniqueDetachableEmscriptenVal(FunctionPointer pfunc, FirstArgument arg0) noexcept : emscripten::val(emscripten::val::undefined()) {
-        // See comments about correctness in js_callback.cpp.
+// See comments about memory correctness in js_callback.cpp.
+struct RequireRelaxedPointerSafety {
+    RequireRelaxedPointerSafety() {
         _ASSERT(std::get_pointer_safety() == std::pointer_safety::preferred || std::get_pointer_safety() == std::pointer_safety::relaxed);
-        emscripten::val::operator=(emscripten::val::module_property("tc_js_callback_detail_js_CreateJsFunction")(reinterpret_cast<PointerNumber>(pfunc), reinterpret_cast<PointerNumber>(arg0)));
     }
+};
 
-    ~CUniqueDetachableEmscriptenVal() {
-        call<void>("detach");
+// We do not care about slicing to js_ref<>, because this class is
+// only stored as a by-value field.
+template<typename T>
+struct CUniqueDetachableJsFunction : tc::nonmovable, RequireRelaxedPointerSafety, js_ref<T> { // TODO: private inheritance?
+    CUniqueDetachableJsFunction(FunctionPointer pfunc, FirstArgument arg0) noexcept : js_ref<T>(
+        emscripten::val::module_property("tc_js_callback_detail_js_CreateJsFunction")(reinterpret_cast<PointerNumber>(pfunc), reinterpret_cast<PointerNumber>(arg0))
+    ) {}
+
+    ~CUniqueDetachableJsFunction() {
+        this->get().template call<void>("detach");
     }
 };
 } // namespace no_adl
-using no_adl::CUniqueDetachableEmscriptenVal;
+using no_adl::CUniqueDetachableJsFunction;
 } // namespace callback_detail
+
+template<typename T>
+struct IsJsRef<callback_detail::CUniqueDetachableJsFunction<T>> : std::true_type {
+    using value_type = IJsFunction<T>;
+};
 
 #define TC_JS_UNIQUE_NAME_IMPL2(Prefix, Line, Counter) Prefix##_##Line##_##Counter
 #define TC_JS_UNIQUE_NAME_IMPL1(Prefix, Line, Counter) TC_JS_UNIQUE_NAME_IMPL2(Prefix, Line, Counter)
 #define TC_JS_UNIQUE_NAME(Prefix) TC_JS_UNIQUE_NAME_IMPL1(Prefix, __LINE__, __COUNTER__)
 
-#define TC_JS_MEMBER_FUNCTION_NAMED_WRAPPER(FieldName, WrapperName, MemberPointer) \
+#define TC_JS_MEMBER_FUNCTION_NAMED_WRAPPER(FieldName, ReturnType, Arguments, WrapperName, MemberPointer) \
     static emscripten::val WrapperName(void* pvThis, emscripten::val const& emvalThis, emscripten::val const& emvalArgs) noexcept { \
         return ::tc::js::callback_detail::MemberFunctionWrapper(MemberPointer, pvThis, emvalThis, emvalArgs); \
     } \
-    const ::tc::js::callback_detail::CUniqueDetachableEmscriptenVal FieldName{&WrapperName, this};
-
-#define TC_JS_MEMBER_FUNCTION_WRAPPER(FieldName, MemberPointer) \
-    TC_JS_MEMBER_FUNCTION_NAMED_WRAPPER(FieldName, TC_JS_UNIQUE_NAME(_tc_js_member_function_wrapper), MemberPointer)
+    const ::tc::js::callback_detail::CUniqueDetachableJsFunction<\
+        ::tc::js::callback_detail::CJsFunctionInterface_t<ReturnType Arguments> \
+    > FieldName{&WrapperName, this};
 
 #define TC_JS_NAMED_MEMBER_FUNCTION(ClassName, FieldName, ReturnType, MethodName, Arguments) \
     void TC_JS_UNIQUE_NAME(_tc_js_member_function_type_check)() { \
         STATICASSERTSAME(ClassName&, decltype(*this), "ClassName specified in TC_JS_NAMED_MEMBER_FUNCTION is incorrect"); \
     } \
-    TC_JS_MEMBER_FUNCTION_WRAPPER(FieldName, &ClassName::MethodName) \
+    TC_JS_MEMBER_FUNCTION_NAMED_WRAPPER(FieldName, ReturnType, Arguments, TC_JS_UNIQUE_NAME(_tc_js_member_function_wrapper), &ClassName::MethodName) \
     ReturnType MethodName Arguments noexcept
 
 #define TC_JS_MEMBER_FUNCTION(ClassName, FieldName, ReturnType, Arguments) \
@@ -220,12 +260,15 @@ template<typename T> struct IsSCallbackResult<SCallbackResult<T>> final : std::t
 } // namespace no_adl
 using no_adl::SCallbackResult;
 using no_adl::IsSCallbackResult;
+
+template<typename Fn>
+using CScopedCallbackBase_t = callback_detail::CUniqueDetachableJsFunction<CJsFunctionInterface_t<boost::callable_traits::function_type_t<Fn>>>;
 } // namespace callback_detail
 
 namespace no_adl {
 // We do not care about slicing to emscripten::val, because this class is only stored by-value.
 template<typename Fn>
-struct CScopedCallback final : callback_detail::CUniqueDetachableEmscriptenVal {
+struct CScopedCallback final : callback_detail::CScopedCallbackBase_t<Fn> {
     static_assert(!std::is_reference<Fn>::value);
     static_assert(
         !callback_detail::IsSCallbackResult<boost::callable_traits::return_type_t<Fn>>::value,
@@ -234,7 +277,7 @@ struct CScopedCallback final : callback_detail::CUniqueDetachableEmscriptenVal {
     static_assert(boost::callable_traits::is_noexcept<Fn>::value, "Callbacks for JS should be noexcept");
 
     template<typename FnSrc>
-    CScopedCallback(FnSrc&& fn) noexcept : CUniqueDetachableEmscriptenVal(&FnWrapper, this), m_fn(std::forward<FnSrc>(fn)) {}
+    CScopedCallback(FnSrc&& fn) noexcept : callback_detail::CScopedCallbackBase_t<Fn>(&FnWrapper, this), m_fn(std::forward<FnSrc>(fn)) {}
 
 private:
     Fn m_fn;
@@ -251,6 +294,9 @@ template<typename Fn> CScopedCallback(Fn) -> CScopedCallback<Fn>;
 } // namespace no_adl
 using no_adl::CScopedCallback;
 
+template<typename Fn>
+struct IsJsRef<CScopedCallback<Fn>> : IsJsRef<callback_detail::CScopedCallbackBase_t<Fn>> {};
+
 namespace callback_detail {
 namespace no_adl {
 template<typename T> struct IsCScopedCallback : std::false_type {};
@@ -259,14 +305,23 @@ template<typename Fn> struct IsCScopedCallback<tc::js::CScopedCallback<Fn>> : st
 using no_adl::IsCScopedCallback;
 } // namespace callback_detail
 
-template<typename Fn> emscripten::val NewHeapCallback(Fn&& fn) noexcept;
+template<typename Fn> auto NewHeapCallback(Fn&& fn) noexcept;
 
 namespace callback_detail {
+template<typename Fn>
+using CHeapCallbackBase_t =
+     callback_detail::CUniqueDetachableJsFunction<CJsFunctionInterface_t<
+         boost::callable_traits::apply_return_t<
+             boost::callable_traits::args_t<Fn>,
+             typename boost::callable_traits::return_type_t<Fn>::value_type
+         >
+     >>;
+
 namespace no_adl {
 // We do not care about slicing to emscripten::val, because this class is only
 // created by NewHeapCallback and deleted by itself.
 template<typename Fn>
-struct CHeapCallback final : CUniqueDetachableEmscriptenVal {
+struct CHeapCallback final : callback_detail::CHeapCallbackBase_t<Fn> {
 private:
     static_assert(!std::is_reference<Fn>::value);
     static_assert(
@@ -276,9 +331,9 @@ private:
     static_assert(boost::callable_traits::is_noexcept<Fn>::value, "Callbacks for JS should be noexcept");
 
     template<typename FnSrc>
-    explicit CHeapCallback(FnSrc&& fn) noexcept : CUniqueDetachableEmscriptenVal(&FnWrapper, this), m_fn(std::forward<FnSrc>(fn)) {}
+    explicit CHeapCallback(FnSrc&& fn) noexcept : CHeapCallbackBase_t<Fn>(&FnWrapper, this), m_fn(std::forward<FnSrc>(fn)) {}
 
-    friend emscripten::val tc::js::NewHeapCallback(Fn&& fn) noexcept;
+    friend auto tc::js::NewHeapCallback(Fn&& fn) noexcept;
 
     Fn m_fn;
     static emscripten::val FnWrapper(void* pvThis, emscripten::val const& emvalThis, emscripten::val const& emvalArgs) noexcept {
@@ -296,6 +351,9 @@ template<typename Fn> CHeapCallback(Fn) -> CHeapCallback<Fn>;
 } // namespace no_adl
 using no_adl::CHeapCallback;
 } // namespace callback_detail
+
+template<typename Fn>
+struct IsJsRef<callback_detail::CHeapCallback<Fn>> : IsJsRef<callback_detail::CHeapCallbackBase_t<Fn>> {};
 
 template<typename T, typename... Args> constexpr auto KeepThisCallback(Args&&... args) noexcept {
     return callback_detail::SCallbackResult<tc::decay_t<T>>(true, std::forward<Args>(args)...);
@@ -322,8 +380,8 @@ inline constexpr auto DeleteThisCallback() noexcept {
 }
 
 template<typename Fn>
-emscripten::val NewHeapCallback(Fn&& fn) noexcept { // TODO: even std::bad_alloc?
-    return *(new callback_detail::CHeapCallback(std::forward<Fn>(fn)));
+auto NewHeapCallback(Fn&& fn) noexcept { // TODO: even std::bad_alloc?
+    return js_ref(*(new callback_detail::CHeapCallback(std::forward<Fn>(fn))));
 }
 
 // ---------------------------------------- Fires-once callback ----------------------------------------
@@ -357,7 +415,7 @@ using no_adl::FiresOnceCallbackWrapper;
 } // namespace new_fires_once_callback_details
 
 template<typename Fn>
-emscripten::val NewFiresOnceCallback(Fn&& fn) noexcept { // TODO: even std::bad_alloc?
+auto NewFiresOnceCallback(Fn&& fn) noexcept { // TODO: even std::bad_alloc?
     return NewHeapCallback(fires_once_callback_detail::FiresOnceCallbackWrapper<
         boost::callable_traits::return_type_t<Fn>,
         Fn,
@@ -365,43 +423,3 @@ emscripten::val NewFiresOnceCallback(Fn&& fn) noexcept { // TODO: even std::bad_
         >(std::forward<Fn>(fn)));
 }
 } // namespace tc::js
-
-// ---------------------------------------- Custom embind marshalling ----------------------------------------
-// Even though CScopedCallback<> and CUniqueDetachableEmscriptenVal are subclasses of emscripten::val,
-// template specializations and embind's bindings are invariant, so we have to provide our own.
-namespace emscripten::internal {
-	template<typename Fn>
-	struct BindingType<tc::js::CScopedCallback<Fn>> {
-		typedef BindingType<val>::WireType WireType;
-		static WireType toWireType(tc::js::CScopedCallback<Fn> const& cbl) {
-			return BindingType<val>::toWireType(cbl);
-		}
-	};
-
-	template<typename T>
-	struct TypeID<T, std::enable_if_t<tc::js::callback_detail::IsCScopedCallback<tc::remove_cvref_t<T>>::value>> {
-		static constexpr TYPEID get() {
-			return TypeID<val>::get();
-		}
-	};
-
-	template<>
-	struct BindingType<tc::js::callback_detail::CUniqueDetachableEmscriptenVal> {
-		typedef BindingType<val>::WireType WireType;
-		static WireType toWireType(tc::js::callback_detail::CUniqueDetachableEmscriptenVal const& v) {
-			return BindingType<val>::toWireType(v);
-		}
-	};
-
-	template<typename T>
-	struct TypeID<T, std::enable_if_t<
-	    std::is_same<
-	        tc::js::callback_detail::CUniqueDetachableEmscriptenVal,
-	        tc::remove_cvref_t<T>
-	    >::value
-	>> {
-		static constexpr TYPEID get() {
-			return TypeID<val>::get();
-		}
-	};
-}
