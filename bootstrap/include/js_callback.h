02a877f64cb5c11b899a3580e9fd56e397c1d686
Egor Suvorov <esuvorov@think-cell.com>
2019-08-19 19:05:26 +0200

    Move IJsFunction from js_types.h to js_callback.h

--- bootstrap/include/js_callback.h
+++ bootstrap/include/js_callback.h
@@ -15,12 +15,36 @@
 #include "tc_move.h"
 #include "type_list.h"
 #include <boost/callable_traits.hpp>
-#include "js_types.h"
+#include "js_ref.h"
 
 namespace tc::js {
 // ---------------------------------------- Passing member functions to JS ----------------------------------------
 DEFINE_TAG_TYPE(pass_this)
 DEFINE_TAG_TYPE(pass_all_arguments)
+
+namespace no_adl {
+template<typename, typename = void, typename = void>
+struct IJsFunction {};
+
+template<typename R, typename... Args, typename TThis, typename TArgs>
+struct IJsFunction<R(Args...), TThis, TArgs> : virtual IJsBase {
+    static_assert(tc::is_decayed<R>::value);
+    static_assert(std::conjunction<tc::is_decayed<Args>...>::value);
+    static_assert(tc::is_decayed<TThis>::value);
+    static_assert(tc::is_decayed<TArgs>::value);
+};
+
+template<typename R, typename... Args>
+struct IJsFunction<R(Args...), void, void> : virtual IJsBase {
+    static_assert(tc::is_decayed<R>::value);
+    static_assert(std::conjunction<tc::is_decayed<Args>...>::value);
+
+    R operator()(Args... args) noexcept {
+        return m_emval(tc_move(args)...).template as<R>();
+    }
+};
+} // namespace no_adl
+using no_adl::IJsFunction;
 
 namespace callback_detail {
 namespace no_adl {
--- bootstrap/include/js_types.h
+++ bootstrap/include/js_types.h
@@ -41,32 +41,10 @@ namespace no_adl {
 struct IAny : virtual IJsBase {
 };
 
-template<typename, typename = void, typename = void>
-struct IJsFunction {};
-
-template<typename R, typename... Args, typename TThis, typename TArgs>
-struct IJsFunction<R(Args...), TThis, TArgs> : virtual IJsBase {
-    static_assert(tc::is_decayed<R>::value);
-    static_assert(std::conjunction<tc::is_decayed<Args>...>::value);
-    static_assert(tc::is_decayed<TThis>::value);
-    static_assert(tc::is_decayed<TArgs>::value);
-};
-
-template<typename R, typename... Args>
-struct IJsFunction<R(Args...), void, void> : virtual IJsBase {
-    static_assert(tc::is_decayed<R>::value);
-    static_assert(std::conjunction<tc::is_decayed<Args>...>::value);
-
-    R operator()(Args... args) noexcept {
-        return m_emval(tc_move(args)...).template as<R>();
-    }
-};
-
 template<typename T>
 struct IsJsIntegralEnum : std::false_type {};
 } // namespace no_adl
 using no_adl::IAny;
-using no_adl::IJsFunction;
 using no_adl::IsJsIntegralEnum;
 } // namespace tc::js
 
