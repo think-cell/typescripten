28961210bb89e75bb85faa76cb58fc07e6e63d3a
Egor Suvorov <esuvorov@think-cell.com>
2019-08-21 13:00:39 +0200

    examples/CallbacksTest: remove unused heap callback tests from main-pre.js

--- examples/CallbacksTest/main-pre.js
+++ examples/CallbacksTest/main-pre.js
@@ -23,27 +23,3 @@ Module.TestPassThisPassAllArgumentsAndReturn = function(callback) {
     const obj = {method: callback, secretField: "secretValue"};
     Module.assertEquals(obj.method(1, "message", null), "returnValue");
 }
-
-Module.TestHeapCallback = function(callback) {
-    Module.assertEquals(callback(2), undefined);
-    Module.assertEquals(callback(1), undefined);
-    Module.assertEquals(callback(0), undefined);
-}
-
-Module.TestHeapCallbackWithReturn = function(callback) {
-    Module.assertEquals(callback(2), 12);
-    Module.assertEquals(callback(1), 11);
-    Module.assertEquals(callback(0), 10);
-}
-
-Module.TestHeapCallbackPassAllArguments = function(callback) {
-    Module.assertEquals(callback(2, null), undefined);
-    Module.assertEquals(callback(1, null), undefined);
-    Module.assertEquals(callback(0, null), undefined);
-}
-
-Module.TestHeapCallbackWithStringReturn = function(callback) {
-    Module.assertEquals(callback(2), "12");
-    Module.assertEquals(callback(1), "11");
-    Module.assertEquals(callback(0), "10");
-}
