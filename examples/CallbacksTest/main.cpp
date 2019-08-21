cb1a9d17bb3155e680f2af0423951cba6e4f6989
Egor Suvorov <esuvorov@think-cell.com>
2019-08-21 17:54:48 +0200

    examples/CallbacksTest: simplify type checks

--- examples/CallbacksTest/main.cpp
+++ examples/CallbacksTest/main.cpp
@@ -11,59 +11,51 @@ using tc::js::pass_this_t;
 using tc::js::pass_all_arguments_t;
 
 #define FOR_ALL_CALLBACKS(CreateCallback) \
-    CreateCallback(TestSum, (js_ref<IJsFunction<int(int, int)>>), int, (int a, int b), { \
+    CreateCallback(TestSum, int, (int a, int b), { \
         _ASSERTEQUAL(a, 10); \
         _ASSERTEQUAL(b, 20); \
         return a + b; \
     }) \
-    CreateCallback(TestPrint, (js_ref<IJsFunction<void(std::string)>>), void, (std::string sMessage), { \
+    CreateCallback(TestPrint, void, (std::string sMessage), { \
         _ASSERTEQUAL(sMessage, "hello"); \
     })\
-    CreateCallback(TestPassAllArguments, (js_ref<IJsFunction<void(pass_all_arguments_t, val, int)>>), void, (pass_all_arguments_t, val emvalArgs, int a), { \
+    CreateCallback(TestPassAllArguments, void, (pass_all_arguments_t, val emvalArgs, int a), { \
         _ASSERTEQUAL(a, 1); \
         _ASSERTEQUAL(emvalArgs[1].as<std::string>(), "message"); \
         _ASSERTEQUAL(emvalArgs["length"].as<int>(), 3); \
     }) \
-    CreateCallback(TestPassThis, (js_ref<IJsFunction<void(pass_this_t, val, int, std::string, val)>>), void, (pass_this_t, val emvalThis, int a, std::string b, val c), { \
+    CreateCallback(TestPassThis, void, (pass_this_t, val emvalThis, int a, std::string b, val c), { \
         _ASSERTEQUAL(emvalThis["secretField"].as<std::string>(), "secretValue"); \
         _ASSERTEQUAL(a, 1); \
         _ASSERTEQUAL(b, "message"); \
         _ASSERT(c.isNull()); \
     }) \
-    CreateCallback(TestPassThisPassAllArguments, (js_ref<IJsFunction<void(pass_this_t, val, pass_all_arguments_t, val, int)>>), void, (pass_this_t, val emvalThis, pass_all_arguments_t, val emvalArgs, int a), { \
+    CreateCallback(TestPassThisPassAllArguments, void, (pass_this_t, val emvalThis, pass_all_arguments_t, val emvalArgs, int a), { \
         _ASSERTEQUAL(emvalThis["secretField"].as<std::string>(), "secretValue"); \
         _ASSERTEQUAL(a, 1); \
         _ASSERTEQUAL(emvalArgs[1].as<std::string>(), "message"); \
         _ASSERTEQUAL(emvalArgs["length"].as<int>(), 3); \
     }) \
-    CreateCallback(TestPassAllArgumentsAndReturn, (js_ref<IJsFunction<std::string(pass_all_arguments_t, val, int)>>), std::string, (pass_all_arguments_t, val emvalArgs, int a), { \
+    CreateCallback(TestPassAllArgumentsAndReturn, std::string, (pass_all_arguments_t, val emvalArgs, int a), { \
         _ASSERTEQUAL(a, 1); \
         _ASSERTEQUAL(emvalArgs[1].as<std::string>(), "message"); \
         _ASSERTEQUAL(emvalArgs["length"].as<int>(), 3); \
         return "returnValue"; \
     }) \
-    CreateCallback(TestPassThisAndReturn, (js_ref<IJsFunction<std::string(pass_this_t, val, int, std::string, val)>>), std::string, (pass_this_t, val emvalThis, int a, std::string b, val c), { \
+    CreateCallback(TestPassThisAndReturn, std::string, (pass_this_t, val emvalThis, int a, std::string b, val c), { \
         _ASSERTEQUAL(emvalThis["secretField"].as<std::string>(), "secretValue"); \
         _ASSERTEQUAL(a, 1); \
         _ASSERTEQUAL(b, "message"); \
         _ASSERT(c.isNull()); \
         return "returnValue"; \
     }) \
-    CreateCallback(TestPassThisPassAllArgumentsAndReturn, (js_ref<IJsFunction<std::string(pass_this_t, val, pass_all_arguments_t, val, int)>>), std::string, (pass_this_t, val emvalThis, pass_all_arguments_t, val emvalArgs, int a), { \
+    CreateCallback(TestPassThisPassAllArgumentsAndReturn, std::string, (pass_this_t, val emvalThis, pass_all_arguments_t, val emvalArgs, int a), { \
         _ASSERTEQUAL(emvalThis["secretField"].as<std::string>(), "secretValue"); \
         _ASSERTEQUAL(a, 1); \
         _ASSERTEQUAL(emvalArgs[1].as<std::string>(), "message"); \
         _ASSERTEQUAL(emvalArgs["length"].as<int>(), 3); \
         return "returnValue"; \
     })
-
-template<typename>
-struct UnwrapType {};
-
-template<typename T>
-struct UnwrapType<void(T)> {
-    using type = T;
-};
 
 // Macro so callback creation is performed between "start" and "end".
 // As ExpectedType is wrapped into parenthesis (to templates can be passed),
@@ -75,7 +67,7 @@ struct UnwrapType<void(T)> {
         using Fn = decltype(cb); \
         static_assert(std::is_convertible< \
             tc::remove_cvref_t<Fn>*, \
-            typename UnwrapType<void ExpectedType>::type* \
+            ExpectedType* \
         >::value, #Name); \
         val::module_property(#Name)(std::forward<Fn>(cb)); \
         printf(#Name " end\n"); \
@@ -84,20 +76,20 @@ struct UnwrapType<void(T)> {
 int main() {
     {
         printf("===== js_lambda_wrap =====\n");
-        #define CALL_SCOPED_CALLBACK(Name, ExpectedType, ReturnType, Arguments, Body) \
-            RUN_TEST(Name, ExpectedType, tc::js::js_lambda_wrap([]Arguments noexcept -> ReturnType Body));
+        #define CALL_SCOPED_CALLBACK(Name, ReturnType, Arguments, Body) \
+            RUN_TEST(Name, js_ref<IJsFunction<ReturnType Arguments>>, tc::js::js_lambda_wrap([]Arguments noexcept -> ReturnType Body));
         FOR_ALL_CALLBACKS(CALL_SCOPED_CALLBACK)
         #undef CALL_SCOPED_CALLBACK
     }
     {
         printf("===== TC_JS_MEMBER_FUNCTION =====\n");
-        #define DEFINE_MEMBER(Name, ExpectedType, ReturnType, Arguments, Body) TC_JS_MEMBER_FUNCTION(TestClass, m_jsfn##Name, ReturnType, Arguments) Body
+        #define DEFINE_MEMBER(Name, ReturnType, Arguments, Body) TC_JS_MEMBER_FUNCTION(TestClass, m_jsfn##Name, ReturnType, Arguments) Body
         struct TestClass {
             FOR_ALL_CALLBACKS(DEFINE_MEMBER)
         } obj;
         #undef DEFINE_MEMBER
-        #define CALL_MEMBER(Name, ExpectedType, ReturnType, Arguments, Body) \
-            RUN_TEST(Name, ExpectedType, obj.m_jsfn##Name);
+        #define CALL_MEMBER(Name, ReturnType, Arguments, Body) \
+            RUN_TEST(Name, js_ref<IJsFunction<ReturnType Arguments>>, obj.m_jsfn##Name);
         FOR_ALL_CALLBACKS(CALL_MEMBER)
         #undef CALL_MEMBER
     }
