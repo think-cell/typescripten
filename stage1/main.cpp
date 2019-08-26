83a3c7147dd38ff67b5365d677c12cf7fd663e22
Egor Suvorov <esuvorov@think-cell.com>
2019-08-26 16:18:35 +0200

    Start stage1 TS-->C++ binding generator

---
+++ stage1/main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string>
+#include "range_defines.h"
+#include "range.h"
+#include "typescript.d.bootstrap.h"
+
+using tc::js::js_string;
+using tc::js::globals::ts;
+using tc::js::globals::Array;
+using tc::js::globals::ReadonlyArray;
+
+int main(int argc, char* argv[]) {
+    _ASSERT(2 <= argc);
+
+    ts::CompilerOptions jsCompilerOptions;
+//    jsCompilerOptions->noEmitOnError(true);
+//    jsCompilerOptions->strict(true);
+    jsCompilerOptions->target(ts::ScriptTarget::ES5);
+    jsCompilerOptions->module(ts::ModuleKind::CommonJS);
+
+    auto rngFileNames = tc::make_iterator_range(argv + 1, argv + argc);
+    ts::Program jsProgram = ts()->createProgram(ReadonlyArray<js_string>(rngFileNames), jsCompilerOptions);
+
+    ts::TypeChecker jsTypeChecker = jsProgram->getTypeChecker();
+
+    _ASSERTEQUAL(0, ts()->getPreEmitDiagnostics(jsProgram)->length());
+
+    tc::for_each(
+        tc::filter(
+            jsProgram->getSourceFiles(),
+            [&](ts::SourceFile const& jsSourceFile) {
+                return tc::find_unique<tc::return_bool>(rngFileNames, std::string(jsSourceFile->fileName()));
+            }
+        ),
+        [](ts::SourceFile const& jsSourceFile) {
+            auto jsChildren = jsSourceFile->getChildren();
+            _ASSERTEQUAL(jsChildren->length(), 2);
+            _ASSERTEQUAL(jsChildren[0]->kind(), ts::SyntaxKind::SyntaxList);
+            _ASSERTEQUAL(jsChildren[1]->kind(), ts::SyntaxKind::EndOfFileToken);
+            tc::for_each(jsChildren[0]->getChildren(),
+                [](ts::Node jnodeTopLevel) {
+                    printf("New top level node: %d\n", jnodeTopLevel->kind());
+                }
+            );
+        }
+    );
+    return 0;
+}
---
+++ stage1/run-demo.cmd
@@ -0,0 +1,1 @@
+node main.js node_modules/typescript/lib/typescript.d.ts
