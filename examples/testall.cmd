a049fa90c9275cee182a63a40e962a3e201f4bcf
Egor Suvorov <esuvorov@think-cell.com>
2019-08-29 17:57:24 +0200

    examples/testall: use 'exit /b' instead of 'exit'

--- examples/testall.cmd
+++ examples/testall.cmd
@@ -2,9 +2,9 @@
 for /D %%i in (*) DO (
     echo %%i
     cd %%i
-    call build || exit 1
+    call build || exit /b 1
     if exist test.cmd (
-        call test || exit 1
+        call test || exit /b 1
     )
     cd ..
 )
