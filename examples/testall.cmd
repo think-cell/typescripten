c4c6f1e4869c4c14f4b57937352617e99568c05c
Egor Suvorov <esuvorov@think-cell.com>
2019-08-30 15:49:22 +0200

    examples/testall: call get-dependencies

--- examples/testall.cmd
+++ examples/testall.cmd
@@ -2,6 +2,9 @@
 for /D %%i in (*) DO (
     echo %%i
     pushd %%i
+    if exist get-dependencies.cmd (
+        call get-dependencies || exit /b 1
+    )
     call build || exit /b 1
     if exist test.cmd (
         call test || exit /b 1
