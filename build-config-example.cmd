8a30e37993167e6312a81670b182575d4a989741
Egor Suvorov <egor.suvorov@gmail.com>
2019-12-20 13:12:36 +0300

    build-config: call emsdk_env before building to ensure Python 2 and other bins

--- build-config-example.cmd
+++ build-config-example.cmd
@@ -1,7 +1,8 @@
-@REM Copy this file to build-config.cmd and edit three variables below
-@set EMCXX=<path-to-em++>
+@REM Copy this file to build-config.cmd and edit three lines below
+@call <path-to-emsdk_env.bat> >nul 2>&1 || exit /b 1
 @set BOOST_PATH=<path-to-boost-includes>
 @set THINK_CELL_PUBLIC_PATH=<path-to-think-cell-public-library-includes>
+@set EMCXX=em++
 @set BOOTSTRAP_PATH=%~dp0\bootstrap
 @set EMCXX_FLAGS=^
     -s SAFE_HEAP=1 -s ASSERTIONS=2 -s WASM=1 -s DEMANGLE_SUPPORT=1 ^
--- build-config-example.sh
+++ build-config-example.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Copy this file to build-config.cmd and edit three variables below
-EMCXX=<path-to-em++>
+# Copy this file to build-config.cmd and edit three lines below
+source <path-to-emsdk_env.sh> >/dev/null 2>&1 || exit 1
 BOOST_PATH=<path-to-boost-includes>
 THINK_CELL_PUBLIC_PATH=<path-to-think-cell-public-library-includes>
+EMCXX=em++
 BOOTSTRAP_PATH=$(dirname "$0")/bootstrap
 EMCXX_FLAGS="\
     -s SAFE_HEAP=1 -s ASSERTIONS=2 -s WASM=1\
