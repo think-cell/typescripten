d0ab221e60308e0941e74c56ee5ae557ebf85a6e
Egor Suvorov <egor.suvorov@gmail.com>
2020-02-11 22:11:27 +0300

    JsUnionFail: check TypeError more rigorously; improve logging

--- examples/JsUnionFail/main-pre.js
+++ examples/JsUnionFail/main-pre.js
@@ -3,14 +3,14 @@ Module.printErr = function() {}
 
 Module.onRuntimeInitialized = function() {
 	function expectAssertionFailed(strFunc) {
-		console.log('Ensuring that an assertion inside ' + strFunc + ' fails...');
+		console.log('Ensuring that an assertion inside ' + strFunc + ' or type conversion fails...');
 		try {
 			Module[strFunc]();
 			throw new Error(strFunc + ' did not throw an error');
 		} catch (e) {
 			if (e.name === 'RuntimeError' && e.message.indexOf('Assertion failed:') !== -1) {
 				// Do nothing, as expected.
-			} else if (e.name === 'TypeError') {
+			} else if (e.name === 'TypeError' && (e.message === 'Cannot convert "undefined" to double' || e.message === 'Cannot convert "null" to double')) {
 				// Do nothing, as expected.
 			} else {
 				throw e;
