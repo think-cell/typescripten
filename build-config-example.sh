d36bce058d17950d77d1dbe1075896c8b49533f7
Egor Suvorov <egor.suvorov@gmail.com>
2020-03-20 15:20:29 +0300

    build-config-example.sh: export settings

--- build-config-example.sh
+++ build-config-example.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Copy this file to build-config.cmd and edit three lines below
 source <path-to-emsdk_env.sh> >/dev/null 2>&1 || exit 1
-BOOST_PATH=<path-to-boost-includes>
-THINK_CELL_PUBLIC_PATH=<path-to-think-cell-public-library-includes>
-EMCXX=em++
-BOOTSTRAP_PATH=$(dirname "$0")/bootstrap
-EMCXXFLAGS="\
+export BOOST_PATH=<path-to-boost-includes>
+export THINK_CELL_PUBLIC_PATH=<path-to-think-cell-public-library-includes>
+export EMCXX=em++
+export BOOTSTRAP_PATH=$(dirname "$0")/bootstrap
+export EMCXXFLAGS="\
 	-D_DEBUG -Wall -Wextra -Werror\
 	-g -s SAFE_HEAP=1 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1"
-EMLDFLAGS="\
+export EMLDFLAGS="\
     -g4 --source-map-base http://localhost:8080/"
