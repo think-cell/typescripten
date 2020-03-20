167aa632f067aedcbfa8e0096671528d9480751d
Egor Suvorov <egor.suvorov@gmail.com>
2020-03-20 14:41:41 +0300

    build-default.sh: add js_callback; fix mistypes

--- build-default.sh
+++ build-default.sh
@@ -1,9 +1,12 @@
-#!/bin/bash/
+#!/bin/bash
 set -ue
 source "$(dirname ${BASH_SOURCE[0]})/build-config.sh"
 $EMCXX \
     $EMCXXFLAGS -std=c++17\
     -isystem "$THINK_CELL_PUBLIC_PATH"\
     -isystem "$BOOST_PATH"\
-    $EMLDFLAGS --bind
+    $EMLDFLAGS --bind\
+    -I "$BOOTSTRAP_PATH/include"\
+    --pre-js "$BOOTSTRAP_PATH/src/js_callback.js"\
+    "$BOOTSTRAP_PATH/src/js_callback.cpp"\
     "$@"
