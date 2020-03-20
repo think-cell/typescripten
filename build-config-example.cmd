e75f15556fcebe682a29ad3db9ca739bbf8b183a
Egor Suvorov <egor.suvorov@gmail.com>
2020-03-20 13:50:35 +0300

    build-config: split DEFAULT_EMCXXFLAGS in EMCXXFLAGS, EMLDFLAGS, DEFAULT_EMCXXFLAGS

--- build-config-example.cmd
+++ build-config-example.cmd
@@ -4,11 +4,13 @@
 @set THINK_CELL_PUBLIC_PATH=<path-to-think-cell-public-library-includes>
 @set EMCXX=em++
 @set BOOTSTRAP_PATH=%~dp0\bootstrap
+@set EMCXXFLAGS=^
+	-D_DEBUG -Wall -Wextra -Werror ^
+	-g -s SAFE_HEAP=1 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1
+@set EMLDFLAGS=^
+    -g4 --source-map-base http://localhost:8080/
 @set DEFAULT_EMCXXFLAGS=^
-    -s SAFE_HEAP=1 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1 ^
-    -D_DEBUG ^
-    -Wall -Wextra -Werror ^
-    -g4 --source-map-base http://localhost:8080/ ^
-    -std=c++17 --bind ^
+    %EMCXXFLAGS% -std=c++17 ^
     -isystem %THINK_CELL_PUBLIC_PATH% ^
-    -isystem %BOOST_PATH%
+    -isystem %BOOST_PATH% ^
+    %EMLDFLAGS% --bind
--- build-config-example.sh
+++ build-config-example.sh
@@ -5,11 +5,13 @@ BOOST_PATH=<path-to-boost-includes>
 THINK_CELL_PUBLIC_PATH=<path-to-think-cell-public-library-includes>
 EMCXX=em++
 BOOTSTRAP_PATH=$(dirname "$0")/bootstrap
+EMCXXFLAGS="\
+	-D_DEBUG -Wall -Wextra -Werror\
+	-g -s SAFE_HEAP=1 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1"
+EMLDFLAGS="\
+    -g4 --source-map-base http://localhost:8080/"
 DEFAULT_EMCXXFLAGS="\
-    -s SAFE_HEAP=1 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1\
-    -D_DEBUG\
-    -Wall -Wextra -Werror\
-    -g4 --source-map-base http://localhost:8080/\
-    -std=c++17 --bind\
+    $EMCXXFLAGS -std=c++17\
     -isystem $THINK_CELL_PUBLIC_PATH\
-    -isystem $BOOST_PATH"
+    -isystem $BOOST_PATH\
+    $EMLDFLAGS --bind"
