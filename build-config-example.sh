322de4b88858e351d7708334c0f5c1760d7192a2
Egor Suvorov <egor.suvorov@gmail.com>
2020-03-20 15:21:50 +0300

    build-config-example.sh: use BASH_SOURCE for BOOTSTRAP_PATH instead of bash

--- build-config-example.sh
+++ build-config-example.sh
@@ -4,7 +4,7 @@ source <path-to-emsdk_env.sh> >/dev/null 2>&1 || exit 1
 export BOOST_PATH=<path-to-boost-includes>
 export THINK_CELL_PUBLIC_PATH=<path-to-think-cell-public-library-includes>
 export EMCXX=em++
-export BOOTSTRAP_PATH=$(dirname "$0")/bootstrap
+export BOOTSTRAP_PATH=$(dirname "${BASH_SOURCE[0]}")/bootstrap
 export EMCXXFLAGS="\
 	-D_DEBUG -Wall -Wextra -Werror\
 	-g -s SAFE_HEAP=1 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1"
