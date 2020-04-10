64a2cb336188077c56045079f977fc0e0ab9d675
Egor Suvorov <egor.suvorov@gmail.com>
2020-04-10 14:59:05 +0300

    stage1: add tests/run-browser-global

---
+++ stage1/tests/run-browser-global/MyLib.ts
@@ -0,0 +1,35 @@
+function sumOfTwo(a: number, b: number): number {
+    return a + b;
+}
+
+var initially10: number = 10;
+
+class SomeBaseClass {
+    foo(x: number): string { return "base"; }
+}
+
+class SomeObject extends SomeBaseClass {
+    constructor() { super(); } // TODO: is it implicitly defined?
+    str: string = "";
+    arr: Array<SomeObject> = [];
+
+    foo(x: number) { return "foo() retval " + (typeof x) + " " + x; }
+}
+
+enum SomeEnum {
+    ENUM10 = 10,
+    ENUM20 = 20,
+    ENUM21
+}
+
+namespace SomeModule {
+    export function sumOfThree(a: number, b:number, c: number): number {
+        return a + b + c;
+    }
+}
+
+function createObject(a: string): SomeObject {
+    const x = new SomeObject();
+    x.str = a;
+    return x;
+}
---
+++ stage1/tests/run-browser-global/main.cpp
@@ -0,0 +1,34 @@
+#include "../../precompiled.h"
+#include "MyLib.d.h"
+
+int main() {
+	_ASSERTEQUAL(30, tc::js::globals::sumOfTwo(10, 20));
+
+	_ASSERTEQUAL(10, tc::js::globals::initially10());
+	tc::js::globals::initially10(12);
+	_ASSERTEQUAL(12, tc::js::globals::initially10());
+
+	tc::js::globals::SomeObject obj;
+	_ASSERTEQUAL("", tc::explicit_cast<std::string>(obj->str()));
+	_ASSERTEQUAL(0, obj->arr()->length());
+	_ASSERT(tc::empty(obj->arr()));
+	_ASSERTEQUAL("foo() retval number 10", tc::explicit_cast<std::string>(obj->foo(10)));
+
+	tc::js::globals::SomeBaseClass objBase = obj;
+	_ASSERT(objBase.getEmval().strictlyEquals(obj.getEmval()));
+	_ASSERTEQUAL("foo() retval number 10", tc::explicit_cast<std::string>(objBase->foo(10)));
+
+	static_assert(std::is_enum<tc::js::globals::SomeEnum>::value);
+	static_assert(10 == static_cast<int>(tc::js::globals::SomeEnum::ENUM10));
+	static_assert(20 == static_cast<int>(tc::js::globals::SomeEnum::ENUM20));
+	static_assert(21 == static_cast<int>(tc::js::globals::SomeEnum::ENUM21));
+
+	_ASSERTEQUAL(60, tc::js::globals::SomeModule::sumOfThree(10, 20, 30));
+
+	tc::js::globals::SomeObject obj2 = tc::js::globals::createObject(tc::js::js_string("foobar"));
+	_ASSERTEQUAL("foobar", tc::explicit_cast<std::string>(obj2->str()));
+	_ASSERTEQUAL(0, obj2->arr()->length());
+	_ASSERT(tc::empty(obj2->arr()));
+
+	printf("Success!\n");
+}
