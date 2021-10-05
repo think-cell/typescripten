99068b0e311edb452ea4850331fb85d3b4ac2def
Sebastian Theophil <stheophil@think-cell.com>
2021-10-05 15:03:43 +0200

    Remove scratch compiler

--- scratch-compiler/scratch-module.ts
+++
@@ -1,8 +0,0 @@
-namespace MyModule {
-    export namespace Ns {
-        export var a: (number, b: number) => number;
-        export function foo(func: (a: number, b: number) => number) { }
-    }
-}
-
-export = MyModule;
