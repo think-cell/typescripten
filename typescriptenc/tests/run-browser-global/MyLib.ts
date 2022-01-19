84dc4e4d4eb52e00a2e274b61cf454f1c1a84f5c
Sebastian Theophil <stheophil@think-cell.com>
2022-01-19 13:37:12 +0100

    Add tests/run-browser-global

--- typescriptenc/tests/run-browser-global/MyLib.ts
+++ typescriptenc/tests/run-browser-global/MyLib.ts
@@ -1,44 +1,48 @@
-function sumOfTwo(a: number, b: number): number {
-    return a + b;
-}
+namespace MyLib {
+    export function sumOfTwo(a: number, b: number): number {
+        return a + b;
+    }
 
-var initially10: number = 10;
-var initially10: number;  // Duplicated declarations are allowed.
+    export var initially10: number = 10;
+    // export var initially10: number;  // Duplicated declarations are allowed.
 
-interface MyInterface {
-}
+    export interface MyInterface {
+    }
 
-class SomeBaseClass implements MyInterface {
-    foo(x: number): string { return "base"; }
-}
+    export class SomeBaseClass implements MyInterface {
+        foo(x: number): string { return "base"; }
+    }
 
-class SomeObject extends SomeBaseClass {
-    constructor() { super(); } // TODO: is it implicitly defined?
-    str: string = "";
-    arr: Array<SomeObject> = [];
+    export class SomeObject extends SomeBaseClass {
+        constructor() { super(); } // TODO: is it implicitly defined?
+        str: string = "";
+        arr: Array<SomeObject> = [];
 
-    foo(x: number) { return "foo() retval " + (typeof x) + " " + x; }
-}
+        foo(x: number) { return "foo() retval " + (typeof x) + " " + x; }
+    }
 
-enum SomeEnum {
-    ENUM10 = 10,
-    ENUM20 = 20,
-    ENUM21
-}
+    export enum SomeEnum {
+        ENUM10 = 10,
+        ENUM20 = 20,
+        ENUM21
+    }
 
-namespace SomeModule {
-    export function sumOfThree(a: number, b:number, c: number): number {
-        return a + b + c;
+    export namespace SomeModule {
+        export function sumOfThree(a: number, b:number, c: number): number {
+            return a + b + c;
+        }
+    }
+
+    export function createObject(a: string): SomeObject {
+        const x = new SomeObject();
+        x.str = a;
+        return x;
+    }
+
+    export function appendToObjectStr(a: SomeObject): SomeObject {
+        a.str += "x";
+        return a;
     }
 }
 
-function createObject(a: string): SomeObject {
-    const x = new SomeObject();
-    x.str = a;
-    return x;
-}
-
-function appendToObjectStr(a: SomeObject): SomeObject {
-	a.str += "x";
-	return a;
-}
+export = MyLib;
--- typescriptenc/tests/run-browser-global/main.cpp
+++ typescriptenc/tests/run-browser-global/main.cpp
@@ -2,41 +2,41 @@
 #include "MyLib.d.h"
 
 int main() {
-	_ASSERTEQUAL(tc::js::sumOfTwo(10, 20), 30);
+	_ASSERTEQUAL(tc::js::MyLib::sumOfTwo(10, 20), 30);
 
-	_ASSERTEQUAL(tc::js::initially10(), 10);
-	tc::js::initially10(12);
-	_ASSERTEQUAL(tc::js::initially10(), 12);
+	_ASSERTEQUAL(tc::js::MyLib::initially10(), 10);
+	tc::js::MyLib::initially10(12);
+	_ASSERTEQUAL(tc::js::MyLib::initially10(), 12);
 
-	tc::js::SomeObject obj(tc::jst::create_js_object);
+	tc::js::MyLib::SomeObject obj(tc::jst::create_js_object);
 	_ASSERTEQUAL(tc::explicit_cast<std::string>(obj->str()), "");
 	_ASSERTEQUAL(obj->arr()->length(), 0);
 	_ASSERT(tc::empty(obj->arr()));
 	_ASSERTEQUAL(tc::explicit_cast<std::string>(obj->foo(10)), "foo() retval number 10");
 
-	tc::js::SomeBaseClass objBase = obj;
+	tc::js::MyLib::SomeBaseClass objBase = obj;
 	_ASSERT(objBase.getEmval().strictlyEquals(obj.getEmval()));
 	_ASSERTEQUAL(tc::explicit_cast<std::string>(objBase->foo(10)), "foo() retval number 10");
 
-	static_assert(std::is_enum<tc::js::SomeEnum>::value);
-	static_assert(10 == static_cast<int>(tc::js::SomeEnum::ENUM10));
-	static_assert(20 == static_cast<int>(tc::js::SomeEnum::ENUM20));
-	static_assert(21 == static_cast<int>(tc::js::SomeEnum::ENUM21));
+	static_assert(std::is_enum<tc::js::MyLib::SomeEnum>::value);
+	static_assert(10 == static_cast<int>(tc::js::MyLib::SomeEnum::ENUM10));
+	static_assert(20 == static_cast<int>(tc::js::MyLib::SomeEnum::ENUM20));
+	static_assert(21 == static_cast<int>(tc::js::MyLib::SomeEnum::ENUM21));
 
-	_ASSERTEQUAL(tc::js::SomeModule::sumOfThree(10, 20, 30), 60);
+	_ASSERTEQUAL(tc::js::MyLib::SomeModule::sumOfThree(10, 20, 30), 60);
 
-	tc::js::SomeObject obj2 = tc::js::createObject(tc::js::string("foobar"));
+	tc::js::MyLib::SomeObject obj2 = tc::js::MyLib::createObject(tc::js::string("foobar"));
 	_ASSERTEQUAL(tc::explicit_cast<std::string>(obj2->str()), "foobar");
 	_ASSERTEQUAL(obj2->arr()->length(), 0);
 	_ASSERT(tc::empty(obj2->arr()));
 
-	tc::js::SomeObject obj3 = tc::js::appendToObjectStr(obj2);
+	tc::js::MyLib::SomeObject obj3 = tc::js::MyLib::appendToObjectStr(obj2);
 	_ASSERT(obj3.getEmval().strictlyEquals(obj2.getEmval()));
 	_ASSERTEQUAL(tc::explicit_cast<std::string>(obj3->str()), "foobarx");
 	_ASSERTEQUAL(obj3->arr()->length(), 0);
 	_ASSERT(tc::empty(obj3->arr()));
 
-	tc::js::MyInterface iface = obj3;
+	tc::js::MyLib::MyInterface iface = obj3;
 
 	printf("Success!\n");
 }
